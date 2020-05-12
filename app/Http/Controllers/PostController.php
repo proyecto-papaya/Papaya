5ec1b63928ca5bcb39cb6c0a2ce5e714814081eb
mperezv <mperez18@jaumebalmes.net>
2020-05-12 19:35:55 +0200

    Rename PostController Class

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class postController extends Controller
+class PostController extends Controller
 {
     public function showForm() {
         return view("formulario_post");
