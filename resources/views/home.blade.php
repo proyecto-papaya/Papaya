b2b41c7d859b171b09c66a58ee0a572bddb1b071
mperezv <mperez18@jaumebalmes.net>
2020-05-12 20:22:32 +0200

    Añadir jquery a todas las páginas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+            AQUI VAN LAS CARDS
+        </div>
+        <div class="col-md-2">
+            AQUI VAN LAS SUGERENCIAS.
+            Deberían ser position absolute o algo para que no se muevan con el scroll.
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,11 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://kit.fontawesome.com/e3989053af.js" ></script>
+    <!-- JQuery -->
+    <script
+        src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+        crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
