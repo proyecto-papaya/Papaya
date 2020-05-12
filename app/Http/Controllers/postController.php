899d5a9b9ed6146d7c289d8a1ddb9b25c99ed4ad
juancarlosfcxp <jflores18@jaumebalmes.net>
2020-05-12 20:20:24 +0200

    agregado el idea/workspace.xml a gitignore

--- app/Http/Controllers/postController.php
+++ app/Http/Controllers/postController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Post;
 class postController extends Controller
 {
     public function showForm() {
         return view("formulario_post");
+    }
+
+    public function createPost(Request $request)
+    {
+        $post = Post::Create($request->all());
+        return $post;
     }
 }
