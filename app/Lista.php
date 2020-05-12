7f7395af8c53625a9f5088e276ac0ce644f5ba4c
mperezv <mperez18@jaumebalmes.net>
2020-05-12 18:19:56 +0200

    Lista model

--- app/Lista.php
+++ app/Lista.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lista extends Model
 {
-    //
+    protected $fillable = [
+        "title"
+    ];
+
+    /**Relación N:M con Post
+     *
+     * Un post puede pertenecer a N listas y una lista puede tener M posts
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function listas(){
+        return $this->belongsToMany('App\Lista');
+    }
+
+    /**Relación N:1 con User
+     *
+     * Un usuario tiene N listas pero una lista pertenece a 1 usuario
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
 }
