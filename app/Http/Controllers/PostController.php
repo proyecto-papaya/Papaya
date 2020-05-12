5a9e3ff9454640793448dc79cbd3b18fae5d9172
mperezv <mperez18@jaumebalmes.net>
2020-05-12 19:44:26 +0200

    showHome function with params

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -11,6 +12,7 @@ class PostController extends Controller
     }
 
     public function showHome(){
-        return view('home');
+        $posts = Post::all()->sortByDesc('created_at');
+        return view("home", compact("posts"));
     }
 }
