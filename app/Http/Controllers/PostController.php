d7b54ea7e0723b6609c7f6945a6dd2d0de7ca8e3
juancarlosfcxp <jflores18@jaumebalmes.net>
2020-05-14 21:53:56 +0200

    prueba no hagais caso

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 use App\Archivo;
-
+use Illuminate\Support\Facades\DB;
 
 class postController extends Controller
 {
@@ -22,65 +22,23 @@ class postController extends Controller
         $post->user_id=auth()->id();
 
         $post->save();
-        if(isset($request->file)){
-            $file = new Archivo();
-            $file->name=$request->file("file")->getClientOriginalName();
 
-            $file->path=$request->file("file")->store("public");
-            $file->user_id=auth()->id();
-            $file->post_id=$post->id;
+        $file = new Archivo();
+        $file->name=$request->file("file")->getClientOriginalName();
 
-            $file->save();
-        }
+        $file->path=$request->file("file")->store("public");
+        $file->user_id=auth()->id();
+        $file->post_id=$post->id;
 
+        $file->save();
         return redirect()->route('home');
     }
 
     public function showFormEditar($id) {
-        $post=Post::query()
+        $detalle=evento::query()
             ->where('id', $id)
             ->first();
-        $file=Archivo::query()
-            ->where('post_id', $id)
-            ->first();
-        return view("posts.formulario_editar_post",compact('post'),compact('file'));
+        return view("eventoForm",compact('detalle'));
     }
 
-    public function updatePost($id,Request $request) {
-        $post=Post::query()
-            ->where('id', $id)
-            ->first();
-        $post->title=$request->input("title");
-        $post->private=$request->input("private")?1:0;
-        $post->text=$request->input("description");
-
-        $post->save();
-        if(isset($request->file)){
-            $file = Archivo::query()
-                ->where('post_id', $id)
-                ->first();
-            if(isset($file)){
-                Storage::delete($file->path);
-            }else{
-                $file = new Archivo();
-                $file->user_id=auth()->id();
-                $file->post_id=$post->id;
-            }
-
-            $file->name=$request->file("file")->getClientOriginalName();
-            $file->path=$request->file("file")->store("public");
-
-            $file->save();
-        }
-
-        return redirect()->route('home');
-
-    }
-    public function deletePost($id)
-    {
-        $post = post::query()
-            ->where('id', $id)
-            ->first();
-        $post->delete();
-    }
 }
