c9c07ac657d1bd5d0fef1c66fa39c6aa7ebb700e
mperezv <mperez18@jaumebalmes.net>
2020-05-14 23:16:41 +0200

    Esqueleto detalle

--- resources/views/posts/detail.blade.php
+++ resources/views/posts/detail.blade.php
@@ -24,21 +24,44 @@
 
     <div class="row">
         <img src="{{ asset('images/user.png') }}" alt="" class="rounded-circle ml-2" style="width: 2em">
-        <div class="text-dark ml-3">user</div>
+        <div class="text-dark ml-3">{{$post->user->name}}</div>
     </div>
 
     <div class="row mt-5">
         <div class="col-11">
             ARCHIVO
         </div>
-        <div class="col-1">
-            <i class="fas fa-ellipsis-h" onclick="show()"></i>
-                <ul id="dropdown" class="d-none">
-                    <li class="list-unstyled">Descargar</li>
-                    <li class="list-unstyled">Editar</li>
-                    <li class="list-unstyled">Eliminar</li>
-                </ul>
+        @if($post->user->name == Auth::user()->name)
+            <div class="col-1">
+                <i class="fas fa-ellipsis-h" onclick="show()"></i>
+                    <ul id="dropdown" class="d-none">
+                        <li class="list-unstyled">Descargar</li>
+                        <li class="list-unstyled">Editar</li>
+                        <li class="list-unstyled">Eliminar</li>
+                    </ul>
+            </div>
+        @endif
+    </div>
+
+    @if($post->user->name != Auth::user()->name)
+    <div class="row">
+        <div class="btn btn-dark col-10 ml-4">DESCARGAR</div>
+    </div>
+    @endif
+
+    <div class="row">
+        <div class="text-center mt-5">
+            {{$post->text}}
         </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-12">COMENTARIO</div>
+        <div class="col-12">COMENTARIO</div>
+        <div class="col-12">COMENTARIO</div>
+        <div class="col-12">COMENTARIO</div>
+        <div class="col-12">COMENTARIO</div>
+        <div class="col-12">COMENTARIO</div>
     </div>
 </div>
 
