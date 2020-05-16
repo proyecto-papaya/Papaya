63b029625d7bcc5003062799b21ce7a3eacf2910
mperezv <mperez18@jaumebalmes.net>
2020-05-16 17:19:24 +0200

    Añadir icono archivo

--- resources/views/posts/_cards.blade.php
+++ resources/views/posts/_cards.blade.php
@@ -11,7 +11,7 @@
                     <div class="card-text mb-3">{{$post->text}}</div>
                     <div class="card-text row justify-content-between">
                         <div class="col-11 row">
-                            <i class="fas fa-file-pdf col-1"></i>
+                            {{$post->archivos->first()->icon}}
                             <div class="h6 col-6">{{$post->date()}}</div>
                         </div>
                         <i class="far fa-heart col-1"></i>
