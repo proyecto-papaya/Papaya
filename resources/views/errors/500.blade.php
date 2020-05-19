47fdf458bb649ab44655f69a86d088ae9e881edf
anmacovei <amacovei18@jaumebalmes.net>
2020-05-19 12:22:02 +0200

    Estilo vistas 400 y 500

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
-
 @section('content')
-    <h1>500|Error interno del servidor</h1>
+    <div class="d-flex align-items-center m-auto">
+        <div class="col-12 mt-5 pt-5">
+            <h2 class="text-center text-papaya pb-1">500 | Error interno del servidor</h2>
+            <h3 class="text-center pb-3">Ups. Algo ha salido mal</h3>
+            <div class="container">
+                <div class="row">
+                    <div class="col-3 text-center m-auto">
+                        <a class="active" href="/">
+                            <button class="button text-uppercase"><span>Volver a home</span></button></a>
+                    </div>
+                </div>
+            </div>
 @endsection
+
