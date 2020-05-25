692c0d1bf086627aa853a5eaa931942b8f401101
anmacovei <amacovei18@jaumebalmes.net>
2020-05-25 20:11:24 +0200

    restablecer y enviar correo para recuperar contraseña

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+            <div class="card mt-5">
+                <div class="card-header-login">{{ __('Reset Password') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -33,7 +33,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-papaya">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
                             </div>
