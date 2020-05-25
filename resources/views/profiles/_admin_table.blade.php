5161ad14c41b8b72bc924f16881d54bd8b2041d7
mperezv <mperez18@jaumebalmes.net>
2020-05-25 19:35:39 +0200

    Tablas en las vistas para paginación

--- resources/views/profiles/_admin_table.blade.php
+++ resources/views/profiles/_admin_table.blade.php
@@ -1,1 +1,30 @@
+<table class="table table-bordered">
+    <thead>
+    <tr>
+        <th scope="col">ID</th>
+        <th scope="col">NOMBRE</th>
+        <th scope="col">EMAIL</th>
+        <th scope="col">FOTO PERFIL</th>
+        <th scope="col">DESCRIPCIÓN</th>
+        <th scope="col">REMEMBER TOKEN</th>
+        <th scope="col">CREACIÓN</th>
+        <th scope="col">ÚLTIMA ACTUALIZACIÓN</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($users as $user)
+    <tr>
+        <td scope="row">{{ $user->id }}</td>
+        <td>{{ $user->name }}</td>
+        <td>{{ $user->email }}</td>
+        <td>{{ $user->profile_picture }}</td>
+        <td>{{ $user->description }}</td>
+        <td>{{ $user->remember_token }}</td>
+        <td>{{ $user->created_at }}</td>
+        <td>{{ $user->updated_at }}</td>
+    </tr>
+    @endforeach
+    </tbody>
 
+    {{ $users->links() }}
+</table>
