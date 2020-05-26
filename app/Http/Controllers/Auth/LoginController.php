5aa00cbe3a2ed4c56d41b288047062f9802e78f4
anmacovei <amacovei18@jaumebalmes.net>
2020-05-26 10:53:41 +0200

    migración de la bd con la nueva columna last_login en user

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -42,6 +44,11 @@ class LoginController extends Controller
     public function username()
     {
         return 'name';
+    }
+
+    public function authenticated(Request $request, $user) {
+        $user->last_login = Carbon::now()->toDateTimeString();
+        $user->save();
     }
 
 
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,8 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('last_login')->nullable();
+
         });
     }
 
